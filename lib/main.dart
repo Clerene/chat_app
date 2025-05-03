33af288ea9a370537f0b786fd55f15590de355ea
Clerene <clerene212@gmail.com>
2025-05-03 09:00:08 +0700

    Flutter building blocks to know about

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,22 @@ import 'package:flutter/material.dart';
 void main (){
   runApp(ChatApp());
 }
-class ChatApp extends StatelessWidget { const ChatApp({super.key});
-@override
-Widget build (BuildContext context) {
+class ChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+// TODO: implement build
   return MaterialApp(
-    title: "Chat App!!!!",
-    theme: ThemeData(primarySwatch: home: LoginPage(),
+      theme: ThemeData(primarySwatch: Colors.yellow),
+      home: Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Button clicked');
+          },
+          ), // FloatingActionButton
+          body: Text('Hello!'),
+      ), // Scaffold
   ); // MaterialApp
-  Colors.yellow),
 }
-class LoginPage extends StatelessWidget { const LoginPage({ super.key});
-@override
-Widget build (BuildContext context) {
-return Scaffold(
-appBar: AppBar(),
-drawer: Drawer(),
-I
-floatingActionButton: FloatingActionButton (onPressed: () {
-print('Button clicked');
-}), // FloatingActionButton
-body: Text("Hello!"),
-); // Scaffold
 }
