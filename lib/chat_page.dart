df90d1aa31567dd382b61e8b995fd428dd9b2667
Clerene <clerene212@gmail.com>
2025-05-09 13:49:38 +0700

    Building dynamic lists

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
@@ -10,34 +12,38 @@ class ChatPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        elevation: 0,
         title: Text('Hi Pooja!'),
+
         actions: [
           IconButton(
               onPressed: () {
                 print('Icon pressed!');
               },
               icon: Icon(Icons.logout))
-        ],
+        ], // IconButton
       ),
       body: Column(
         children: [
           Expanded(
-      child:ListView(
-        children: [
-          ChatBubble(alignment: Alignment.centerLeft, message: "Hello, this is Pooja!"),
-          ChatBubble(alignment: Alignment.centerRight, message: "Hello, this is Pooja!"),
-        ],
-    ),
-    ),
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index){
+                    return ChatBubble(
+                        alignment: index % 2 == 0
+                            ? Alignment.centerLeft
+                            : Alignment.centerRight,
+                        message: "Hello, this is Pooja!");
+                  })
+            //  child: ListView(
+            //     children: [
+            //      ChatBubble(alignment: Alignment.centerLeft, message: "Hello, this is Poopja!"),
+            //      ChatBubble(alignment: Alignment.centerRight, message: "Hello, this is Poopja!"),
+            //    ],
+            //  ),
+          ),
           ChatInput(),
-    ],
+        ],
       ),
     );
   }
 }
-
-
-
-
-
