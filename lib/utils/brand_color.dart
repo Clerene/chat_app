ea8cd99b9ca606a2c2ddd3f88a32a47a42fc5a41
Clerene <clerene212@gmail.com>
2025-05-13 13:14:14 +0700

    Understanding the anatomy of a pubspec.yaml file

--- lib/utils/brand_color.dart
+++ lib/utils/brand_color.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class BrandColor{
   static Color chatInputColor = const Color(0xff1d1c21);
-  static Color primaryColor = Colors.deepPurple;
+  static MaterialColor primaryColor = Colors.deepPurple;
 }
