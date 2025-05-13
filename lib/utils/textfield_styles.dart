5d8bd3e83dc83bc0ff03802db1da326fae82dc83
Clerene <clerene212@gmail.com>
2025-05-13 20:10:10 +0700

    Exploring pub.dev

--- lib/utils/textfield_styles.dart
+++ lib/utils/textfield_styles.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ThemeTextStyle {
-  static TextStyle LoginTextFieldStyle = TextStyle(color: Colors.blueGrey);
+  static TextStyle LoginTextFieldStyle =
+      GoogleFonts.lato(textStyle: TextStyle(color: Colors.blueGrey));
 }
