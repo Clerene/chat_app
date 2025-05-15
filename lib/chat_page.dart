c4cf326b043ef931e6b88fbcb07db6df0e9c29a1
Clerene <clerene212@gmail.com>
2025-05-15 22:48:28 +0700

    Repository pattern

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
 import 'package:chat_app/models/image_model.dart';
-import 'package:http/http.dart' as http;
 import 'package:chat_app/models/chat_message_entity.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:chat_app/repo/image_repository.dart';
 
 class ChatPage extends StatefulWidget {
   ChatPage({Key? key}) : super(key: key);
@@ -19,22 +19,21 @@ class _ChatPageState extends State<ChatPage> {
 
   _loadInitialMessages() async {
     final response = await rootBundle.loadString('assets/mock_messages.json');
-      final List<dynamic>decodedList = jsonDecode(response) as List;
-      final List<ChatMessageEntity> _chatMessages = decodedList.map((listItem){
-        return ChatMessageEntity.fromJson(listItem);
-      }).toList();
+    final List<dynamic>decodedList = jsonDecode(response) as List;
+    final List<ChatMessageEntity> _chatMessages = decodedList.map((listItem){
+      return ChatMessageEntity.fromJson(listItem);
+    }).toList();
 
-      print(_chatMessages.length);
+    print(_chatMessages.length);
 
-      setState((){
-        _messages = _chatMessages;
-      });
+    setState((){
+      _messages = _chatMessages;
+    });
   }
 
   @override
   void initState() {
     _loadInitialMessages();
-    _getNetworkImages();
     super.initState();
   }
 
@@ -44,21 +43,7 @@ class _ChatPageState extends State<ChatPage> {
     });
   }
 
-  Future<List<PixelfordImage>> _getNetworkImages() async {
-    var endpointUrl = Uri.parse('https://pixelford.com/api2/images');
-
-    final response = await http.get(endpointUrl);
-    if(response.statusCode==200) {
-      final List<dynamic>decodedList = jsonDecode(response.body) as List;
-      final List<PixelfordImage> _imageList = decodedList.map((listItem) {
-        return PixelfordImage.fromJson(listItem);
-      }).toList();
-      print(_imageList[0].urlFullSize);
-      return _imageList;
-    }else{
-      throw Exception('API not successful!');
-    }
-  }
+  final ImageRepository _imageRepo = ImageRepository();
 
   @override
   Widget build(BuildContext context) {
@@ -81,7 +66,7 @@ class _ChatPageState extends State<ChatPage> {
       body: Column(
         children: [
           FutureBuilder<List<PixelfordImage>>(
-              future: _getNetworkImages(),
+              future: _imageRepo.getNetworkImages(),
               builder: (BuildContext context, AsyncSnapshot<List<PixelfordImage>> snapshot){
                 if(snapshot.hasData)
                   return Image.network(snapshot.data![0].urlSmallSize);
